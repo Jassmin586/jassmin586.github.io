e0b9b0a946c3f500e0c5a49efc208fa6e054bf65
Joanna Rządkowska <joannarz52@gmail.com>
2016-11-14 01:37:09 +0100

    Add mobile version

--- BP Palma/js/script.js
+++ BP Palma/js/script.js
@@ -2,9 +2,48 @@ $(document).ready( function(){
 
   $("nav ul li a").click( function() {
     var i = $('nav ul li a').index(this)+1;
-    $("html, body").animate( {
-      scrollTop: $("section:nth-of-type("+i+")").offset().top
+    if(i<=4){
+      if($(window).width()>768){
+          $("html,body").animate({
+          scrollTop: $("section:nth-of-type("+i+")").offset().top-50
+        },400);
+      }
+      else {
+        $("html, body").animate( {
+            scrollTop: $("section:nth-of-type("+i+")").offset().top
+        }, 400);
+      }
+    }
+    else {
+      $("html, body").animate( {
+          scrollTop: $("footer").offset().top
+      }, 400);
+    }
+  });
+
+  $("a.mobile-btn").click(function(){
+    if($("header nav").is(":hidden")) {
+      $("header nav").show();
+      $("a.mobile-btn").toggleClass("active");
+    }
+    else {
+      $("header nav").hide();
+      $("a.mobile-btn").removeClass("active");
+    }
+  });
+
+  $("footer a.up").click(function(){
+    $("html, body").animate({
+      scrollTop: 0
     }, 400);
   });
 
+
+    if($(document).scrollTop() > $("main .catalog").offset().top)
+    {
+      $("footer a.up").show();
+    }
+    else {
+      $("footer a.up").hide();
+    }
 });
