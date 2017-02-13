e778929492485a6fbb56341c5e95599d67822fa1
jassmin586 <joannarz52@gmail.com>
2017-02-13 01:39:41 +0100

    Fix error with closing section Add close btn and add animation effect

--- Building Services/js/script.js
+++ Building Services/js/script.js
@@ -19,16 +19,34 @@ setInterval(function(){
 
 var Section = {
   showSection: function(e) {
-    var name = e.target.getAttribute("href");
+    var elTarget = e.target;
+    var el = elTarget.hasAttribute("href") ? elTarget : elTarget.parentNode;
+    var sectionName = el.getAttribute("href");
+    var handler = document.getElementsByTagName("main")[0];
+
+    e.preventDefault();
+    handler.style.display = "flex";
+    document.querySelector(sectionName).style.display = "flex";
+  },
+  hideSection: function(e){
+    var section = document.getElementsByTagName("section");
     var main = document.getElementsByTagName("main")[0];
-    main.style.display = "flex";
-    document.querySelector(name).style.display = "flex";
+    var elTarget = e.target;
+
+    if (elTarget === main || elTarget.className === "close-btn" || elTarget.parentNode.className === "close-btn") {
+      main.style.display = "none";
+      for(i=0, x=section.length;i<x;i++){
+        section[i].style.display = "none";
+      }
+    }
   },
   events: function(){
     var handler = document.getElementsByClassName("menu-link");
+
     for(i=0, x=handler.length;i<x;i++){
       handler[i].addEventListener("click", Section.showSection);
     }
+    document.getElementsByTagName("main")[0].addEventListener("click", Section.hideSection);
   }
 };
 
