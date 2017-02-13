2fc56e0b2e29e4634f572026c117d672e0b8e9d1
jassmin586 <joannarz52@gmail.com>
2017-02-14 00:21:30 +0100

    Complete 'about us' section.

--- Building Services/js/script.js
+++ Building Services/js/script.js
@@ -18,36 +18,32 @@ setInterval(function(){
 }, 4000);
 
 var Section = {
+  activeSection: "",
   showSection: function(e) {
-    var elTarget = e.target;
-    var el = elTarget.hasAttribute("href") ? elTarget : elTarget.parentNode;
-    var sectionName = el.getAttribute("href");
-    var handler = document.getElementsByTagName("main")[0];
+    if (e.target.classList.contains("menu-link") || e.target.parentNode.classList.contains("menu-link")){
 
-    e.preventDefault();
-    handler.style.display = "flex";
-    document.querySelector(sectionName).style.display = "flex";
+      var el = e.target.hasAttribute("href") ? e.target : e.target.parentNode;
+      var sectionName = el.getAttribute("href");
+      var section = document.querySelector(sectionName);
+      var main = document.getElementsByTagName("main")[0];
+      Section.activeSection = section;
+
+      e.preventDefault();
+      main.className = "open";
+      section.className = "open";
+  }
   },
   hideSection: function(e){
-    var section = document.getElementsByTagName("section");
-    var main = document.getElementsByTagName("main")[0];
-    var elTarget = e.target;
+    if (e.target === e.currentTarget || e.target.className === "close-btn") {
 
-    if (elTarget === main || elTarget.className === "close-btn" || elTarget.parentNode.className === "close-btn") {
-      main.style.display = "none";
-      for(i=0, x=section.length;i<x;i++){
-        section[i].style.display = "none";
-      }
+      Section.activeSection.classList.remove("open");
+      e.currentTarget.classList.remove("open");
     }
   },
-  events: function(){
-    var handler = document.getElementsByClassName("menu-link");
-
-    for(i=0, x=handler.length;i<x;i++){
-      handler[i].addEventListener("click", Section.showSection);
+    events: function(){
+      document.getElementsByTagName("nav")[0].addEventListener("click", Section.showSection);
+      document.getElementsByTagName("main")[0].addEventListener("click", Section.hideSection);
     }
-    document.getElementsByTagName("main")[0].addEventListener("click", Section.hideSection);
-  }
-};
+  };
 
-Section.events();
+  Section.events();
